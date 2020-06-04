9d8d6a4b52b8db663eb48ce3db641c416b753ff4
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2020-06-04 16:57:32 -0600

    algo

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Switch } from 'react-router-dom';
 
+import LoginForm from './auth/LoginForm';
+import RegisterForm from './auth/RegisterForm';
+import PrivateRoute from './common/PrivateRoute';
+
+import { loadUser } from '../actions/auth';
+
 import history from '../history';
 import Header from './layout/Header';
 import Dashboard from './todos/Dashboard';
@@ -13,15 +19,21 @@ import { Provider } from 'react-redux';
 import store from '../store';
 
 class App extends Component {
+  componentDidMount(){
+    store.dispatch(loadUser());
+  }
+
   render() {
     return (
       <Provider store={store}>
         <Router history={history}>
           <Header />
           <Switch>
-            <Route exact path='/' component={Dashboard} />
+            <PrivateRoute exact path='/' component={Dashboard} />
             <Route exact path='/delete/:id' component={TodoDelete} />
             <Route exact path='/edit/:id' component={TodoEdit} />
+            <Route exact path='/login' component={LoginForm} />
+            <Route exact path='/register' component={RegisterForm}/>
           </Switch>
         </Router>
       </Provider>
