e4ce23baeaa318e1528c5793bd2d259b96e86019
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2020-06-04 19:00:13 -0600

    .

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,6 +5,8 @@ from .views import index, TodoDetailView
 
 urlpatterns = [
     path('', index),
+    path('login', index),
+    path('register', index),
     path('edit/<int:pk>', TodoDetailView.as_view()),
     path('delete/<int:pk>', TodoDetailView.as_view()),
 ]
